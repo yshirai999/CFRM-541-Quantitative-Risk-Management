ae355c5d12db362ddd8e7d2a822f822bcbe9b0c9
yshirai999 <yshirai@uw.edu>
2024-11-03 10:34:04 -0500

    deleted basics

--- Basics.R
+++
@@ -1,9 +0,0 @@
-## load data
-library("fBasics")
-data("DowJones30")
-x = DowJones30[,2:31]
-## Compute correlation matrix
-R = cor(x)
-r <- eigen(R)
-V <- r$vectors
-lam <- r$values ##R = V lam V^-1
