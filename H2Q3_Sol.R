d783f176459da95ce1eda8222cd502e5d242aac8
yshirai999 <yshirai@uw.edu>
2024-11-03 16:27:51 -0500

    h2q3

--- H2Q3_Sol.R
+++ H2Q3_Sol.R
@@ -8,16 +8,16 @@ shell("cls")
 library(data.table) # recall to run install.packages("data.table")
 data <- fread("USRiskFreeRatesWeekly.csv", header = TRUE)
 dates <- as.Date(as.matrix(data[, 1]))
-t0 <- as.Date("2007-01-01")
-t1 <- as.Date("2007-12-31")
-t0 <- which(abs(dates - t0) == min(abs(dates - t0)))
+t0 <- as.Date("2007-01-01") #initial date
+t1 <- as.Date("2007-12-31") #final date
+t0 <- which(abs(dates - t0) == min(abs(dates - t0))) # find row
 t1 <- which(abs(dates - t1) == min(abs(dates - t1)))
 r <- as.matrix(data[t0:t1, 2:ncol(data)])
 n <- nrow(r)
 
 ## PCA
-delta_r <- r[2:n, ] - r[1:n - 1, ]
-set.seed(1)
+delta_r <- r[2:n, ] - r[1:n - 1, ] #PCA performed on changes
+set.seed(1) #set seed for reproducibility
 res <- princomp(delta_r, scale = FALSE)
 del <- res$sdev^2
 gam <- res$loadings
